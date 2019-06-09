260e48ccc72fdfd3c279fda21ccf3e7f0f9968c5
matt <mthw.wm.robinson@gmail.com>
2019-06-09 00:27:17 -0400

    updated issue contributors

--- research_utils/research_utils/database/issue_contributors.sql
+++ research_utils/research_utils/database/issue_contributors.sql
@@ -1,14 +1,8 @@
 CREATE MATERIALIZED VIEW open_source.issue_contributors
 AS
-SELECT y.package_id, y.user_id, contributor
-FROM open_source.issues y
-INNER JOIN (
   SELECT x.package_id,
          x.user_id,
-         CASE
-          WHEN commit_pct > 0.05 THEN TRUE
-          ELSE FALSE
-         END as contributor
+        commit_pct
   FROM(
     SELECT a.user_id, a.package_id, CAST(commits AS DECIMAL)/ total_commits AS commit_pct
     FROM open_source.contributors a
@@ -19,5 +13,3 @@ INNER JOIN (
    ) b
    ON a.package_id = b.package_id
   ) x
-) z
-ON (y.package_id = z.package_id AND y.user_id = z.user_id)
