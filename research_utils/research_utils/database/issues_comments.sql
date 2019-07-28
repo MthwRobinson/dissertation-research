0f89ac902ddad8f0f2112aa5f9c32cfeabc14300
matt <mthw.wm.robinson@gmail.com>
2019-07-28 19:05:30 -0400

    changed issue count threshold

--- research_utils/research_utils/database/issues_comments.sql
+++ research_utils/research_utils/database/issues_comments.sql
@@ -2,7 +2,8 @@ DROP MATERIALIZED VIEW open_source.issue_comments;
 CREATE MATERIALIZED VIEW open_source.issue_comments
 AS
 SELECT c.id as comment_id, c.package, c.organization, c.issue_number,
-c.user_id, c.created_at as comment_time, a.created_at as issue_time
+c.user_id, c.created_at as comment_time, a.created_at as issue_time,
+a.id as issue_id
 FROM open_source.issues a
 INNER JOIN (
   SELECT organization, package, total
@@ -19,7 +20,7 @@ INNER JOIN (
 	OR 'enhancement' = ANY(lower(labels::text)::text[]))
     GROUP BY organization, package
 ) x
-WHERE total >= 100
+WHERE total >= 30
 ) b
 ON (a.organization = b.organization
 AND a.package = b.package)
